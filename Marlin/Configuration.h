cc220aaa2d40e660f401a4f174e8ddcaf1f4d7c9
mohdbsaid <mohdbsaid@email.com>
2023-08-01 01:19:15 +0400

    STRING_CONFIG_H_AUTHOR

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,14 +61,14 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(gMax2_SKR2_TMC2209_bltouch,@mohdbsaid)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(@mohdbsaid, gMax2_SKR2_TMC2209_bltouch)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_BTT_SKR_V2_0_REV_B
 #endif
 
 /**
@@ -79,7 +79,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT 0
+#define SERIAL_PORT 1
 
 /**
  * Serial Port Baud Rate
